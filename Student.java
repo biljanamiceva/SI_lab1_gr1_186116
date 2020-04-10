cf9d94ccaddad0b58719e41f73749414c0ca091f
Biljana Miceva <biljana.miceva18@gmail.com>
2020-04-10 19:44:54 +0200

    student class finished

--- Student.java
+++ Student.java
@@ -59,3 +59,12 @@ class Student {
 	}
 
 }
+	class courseFeature {
+	List<Student> lista1=new List<Student>();
+
+	public static void Add(){
+	//TODO
+}
+	public static void brojDobieniPotpisi(){
+	//TODO
+}
